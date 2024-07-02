2230a5d6e95abf040768004ca92cdcc8119fd486
LixNew <129871802+LixNew2@users.noreply.github.com>
2024-07-02 17:12:24 +0200

    Update dll_injector.py

--- src/DLL-Injector/dll_injector.py
+++ src/DLL-Injector/dll_injector.py
@@ -3,7 +3,7 @@ DLL-Injector is a library for injecting DLLs into processes using Python.
 """
 
 __copyright__  = """
-Copyright (c) 2023 LixNew; lixnew2@gmail.com
+Copyright (c) 2024 LixNew; lixnew2@gmail.com
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
